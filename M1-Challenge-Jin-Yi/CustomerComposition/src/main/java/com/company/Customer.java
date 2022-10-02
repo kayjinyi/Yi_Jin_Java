43e9f29605a48afc206c870d4d08dde7dabca4da
kayjinyi <95940771+kayjinyi@users.noreply.github.com>
2022-10-02 02:20:01 -0700

    5

--- M1-Challenge-Jin-Yi/CustomerComposition/src/main/java/com/company/Customer.java
+++ M1-Challenge-Jin-Yi/CustomerComposition/src/main/java/com/company/Customer.java
@@ -2,4 +2,105 @@ package com.company;
 
 public class Customer {
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private boolean rewardMemberOrNot;
+
+    Address shippingAddress = new Address();
+    Address billingAddress = new Address();
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String email, String phoneNumber, String address, boolean rewardMemberOrNot, Address shippingAddress, Address billingAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.rewardMemberOrNot = rewardMemberOrNot;
+        this.shippingAddress = shippingAddress;
+        this.billingAddress = billingAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isRewardMemberOrNot() {
+        return rewardMemberOrNot;
+    }
+
+    public void setRewardMemberOrNot(boolean rewardMemberOrNot) {
+        this.rewardMemberOrNot = rewardMemberOrNot;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                ", rewardMemberOrNot=" + rewardMemberOrNot +
+                ", shippingAddress=" + shippingAddress +
+                ", billingAddress=" + billingAddress +
+                '}';
+    }
 }
