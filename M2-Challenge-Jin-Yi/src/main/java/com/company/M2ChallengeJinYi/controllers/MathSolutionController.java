a18c388f8fccc5e85a558e52fa654a8e2dca7c3f
kayjinyi <95940771+kayjinyi@users.noreply.github.com>
2022-10-12 21:24:09 -0700

    update test

--- M2-Challenge-Jin-Yi/src/main/java/com/company/M2ChallengeJinYi/controllers/MathSolutionController.java
+++ M2-Challenge-Jin-Yi/src/main/java/com/company/M2ChallengeJinYi/controllers/MathSolutionController.java
@@ -2,6 +2,7 @@ package com.company.M2ChallengeJinYi.controllers;
 
 import com.company.M2ChallengeJinYi.models.MathSolution;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,7 +16,9 @@ public class MathSolutionController {
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Integer calculateSum(@RequestBody @Valid MathSolution mathSolution){
-
+        if(mathSolution.getOperand2() == null || mathSolution.getOperand1() == null ){
+            throw new IllegalArgumentException("Operand1 and Operand2 can not be empty");
+        }
         return mathSolution.add();
     }
 
@@ -23,20 +26,28 @@ public class MathSolutionController {
     @RequestMapping(value = "/subtract", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Integer calculateDifference(@RequestBody @Valid MathSolution mathSolution){
-
+        if(mathSolution.getOperand2() == null || mathSolution.getOperand1() == null ){
+            throw new IllegalArgumentException("Operand1 and Operand2 can not be empty");
+        }
         return mathSolution.subtract();
     }
 
     @RequestMapping(value = "/multiply", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Integer calculateProduct(@RequestBody @Valid MathSolution mathSolution){
-
+        if(mathSolution.getOperand2() == null || mathSolution.getOperand1() == null ){
+            throw new IllegalArgumentException("Operand1 and Operand2 can not be empty");
+        }
         return mathSolution.multiply();
     }
 
     @RequestMapping(value = "/divide", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Integer calculateQuotient(@RequestBody @Valid MathSolution mathSolution){
+        if(mathSolution.getOperand2() == null || mathSolution.getOperand1() == null ){
+            throw new IllegalArgumentException("Operand1 and Operand2 can not be empty");
+        }
+
         if(mathSolution.getOperand2() == 0){
             throw new IllegalArgumentException("Zero cannot be denominator");
         }
